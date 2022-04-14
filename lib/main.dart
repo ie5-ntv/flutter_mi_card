012fe806f425c738fa02ad6205b78e9ce78dca69
edduson <bingibergs@gmail.com>
2022-04-14 02:14:12 +0000

    Udemy skilav 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+        )
     );
   }
 }
